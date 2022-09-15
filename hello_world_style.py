df6071fd9b10a6571a64fe52dcbcd9778e04fe51
Frankie Fox Wagoosh <frank.fox-wagoosh@stu.ocsb.ca>
2022-09-15 22:34:32 +0000

    Added hello_world_updated.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#created By:FrankieFox-Wagoosh
+# Date: Sept,15,2022
+# This program displays Hello, World! with proper style.__doc__
+
+
+def main ():
+  print('Hello, World! with proper style')
+  print('I am testing my skills here.')
+
+
+if __name__ == "__main__":
+   main()
